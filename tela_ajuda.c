bb409f831629c6f1d762c72c4a64cdb7480473ef
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-09-04 19:32:55 -0300

    Alteração estética na tela inicial

--- tela_ajuda.c
+++ tela_ajuda.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Cores ANSI
+#define reset   "\033[0m"
+#define negrito    "\033[1m"
+#define ciano     "\033[1;36m"
+#define roxo  "\033[1;35m"
+#define rosa     "\033[38;5;213m"
+#define vermelho     "\033[31m"
+
 void telaAjuda(void) {
-    system("clear"); // limpa a tela (Linux/Unix)
-
+    system("clear"); 
     printf("\n");
-    printf("\033[1;35m==========================================\033[0m\n"); // Magenta
-    printf("\033[1;33m                AJUDA E CONTATO           \033[0m\n"); // Amarelo
-    printf("\033[1;35m==========================================\033[0m\n\n");
+    printf(roxo negrito "✿ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ✿\n"reset); 
+    printf(roxo negrito "★"rosa negrito"          AJUDA E CONTATO          "roxo negrito"★\n"reset); 
+    printf(roxo negrito"✿ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ✿\n\n"reset);
 
-    printf("\033[1;36mComo navegar pelo sistema:\033[0m\n");
+    printf(ciano negrito "✿ Como navegar pelo sistema:\n"reset);
     printf("- Use os números do menu principal para escolher as opções.\n");
     printf("- Pressione ENTER quando solicitado para voltar.\n");
     printf("- Evite usar teclas diferentes dos números indicados.\n\n");
 
-    printf("\033[1;36mPrecisa de suporte?\033[0m\n");
+    printf(ciano negrito "✿ Precisa de suporte?\n"reset);
     printf("- Email: suporte@signails.com\n");
     printf("- WhatsApp: (99) 99999-9999\n");
     printf("- Instagram: @sig_nails\n\n");
-
-    printf("\033[1;35m==========================================\033[0m\n");
-    printf("Pressione ENTER para voltar ao menu...\n");
-    printf("\033[1;35m==========================================\033[0m\n");
-
-    getchar(); // aguarda Enter do usuário
+    printf(rosa "Pressione ENTER para voltar ao menu...\n"reset);
+    getchar(); 
+}
+int main() {
+    telaAjuda();
+    return 0;
 }
--- tela_inicial.c
+++ tela_inicial.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <stdlib.h> 
+#include <ctype.h> 
 
 //Cores ANSI
 #define reset   "\033[0m"
