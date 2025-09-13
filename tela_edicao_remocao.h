41f4fd256809b205fc395a9f2279348bdecd6aca
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-09-13 13:41:22 -0300

    Criação do header de tela_edicao_remocao.h

---
+++ tela_edicao_remocao.h
@@ -0,0 +1,9 @@
+#ifndef TELA_EDICAO_REMOCAO_H
+#define TELA_EDICAO_REMOCAO_H
+void telaComoAgendar(void);
+void telaPrecos(void);
+void telaEditarAgendamento(void);
+void telaNovoAgendamento(void);
+void telaConfirmacao(void);
+void telaRemoverAgendamento(void);
+#endif
