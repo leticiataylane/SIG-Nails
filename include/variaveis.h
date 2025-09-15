8c45894daa07d085ee60e0c5e1fbec902500ed99
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-09-15 10:38:52 -0300

    Criação do header variaveis.h

---
+++ include/variaveis.h
@@ -0,0 +1,8 @@
+#ifndef VARIAVEIS_H
+#define VARIAVEIS_H
+extern char ultimoNome[50];
+extern char ultimoServico[50];
+extern char ultimaData[15];
+extern char ultimaHora[10];
+extern int agendamentoFeito;
+#endif
