bdd37035f984bbbb411065919bfbe599b588fee1
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-09-26 17:29:11 -0300

    refactor: realocação da tela de agendamento.

---
+++ telas/Agendamento/telas_agendamentos.h
@@ -0,0 +1,16 @@
+#ifndef TELAS_AGENDAMENTOS_H 
+#define TELAS_AGENDAMENTOS_H
+
+char modAgendamento(void);
+
+char menuAgendamento(void);
+
+void telaCadastrarAgendamento(void);
+
+void telaAtualizarAgendamento(void);
+
+void telaPesquisarAgendamento(void);
+    
+void telaExcluirAgendamento(void);
+
+#endif
