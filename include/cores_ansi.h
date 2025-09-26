870b7a22227501f89167b2e85f0f03e468489e5d
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-09-26 19:58:38 -0300

    feat: Adiciona header de cores ANSI

---
+++ include/cores_ansi.h
@@ -0,0 +1,14 @@
+#ifndef CORES_ANSI_H
+#define CORES_ANSI_H
+
+#define reset   "\033[0m"
+#define negrito    "\033[1m"
+#define ciano     "\033[1;36m"
+#define roxo  "\033[1;35m"
+#define rosa     "\033[38;5;213m"
+#define verde   "\033[32m"
+#define azul    "\033[34m"
+#define amarelo "\033[33m"
+#define vermelho     "\033[31m"
+
+#endif
