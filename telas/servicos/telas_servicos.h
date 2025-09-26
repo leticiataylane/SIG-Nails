cc7b72e747822d91ac3a7928fa031fc9d7642141
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-09-26 17:30:55 -0300

    feat: criando e alocando telas de serviços.

---
+++ telas/servicos/telas_servicos.h
@@ -0,0 +1,16 @@
+#ifndef TELAS_SERVICOS_H 
+#define TELAS_SERVICOS_H
+
+char modServico(void);
+
+char menuServico(void);
+
+void telaCadastrarServico(void);
+
+void telaAtualizarServico(void);
+
+void telaPesquisarServico(void);
+    
+void telaExcluirServico(void);
+
+#endif
