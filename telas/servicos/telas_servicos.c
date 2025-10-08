c3a5a814bc62104dd0bb9ec577181b3c10d16b7a
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-10-08 18:41:38 -0300

    fix: Ajuste nas funções do CRUD

--- telas/servicos/telas_servicos.c
+++ telas/servicos/telas_servicos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "telas_servicos.h"
 
 typedef struct servico {
     char nome[50];
--- telas/servicos/telas_servicos.h
+++ telas/servicos/telas_servicos.h
@@ -1,16 +1,16 @@
 #ifndef TELAS_SERVICOS_H 
 #define TELAS_SERVICOS_H
 
+typedef struct servico {
+    char nome[50];
+    float preco;
+} Servico;
+
 char modServico(void);
-
 char menuServico(void);
-
 void telaCadastrarServico(void);
-
 void telaAtualizarServico(void);
-
 void telaPesquisarServico(void);
-    
 void telaExcluirServico(void);
 
 #endif
