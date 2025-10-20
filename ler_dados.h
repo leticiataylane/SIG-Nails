f12a1af82005ebc57a12e9b3015319c87f4803cb
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-10-20 10:32:56 -0300

    feat: arquivo para a função de leitura de dados.

---
+++ ler_dados.h
@@ -0,0 +1,24 @@
+#ifndef LER_DADOS_H
+#define LER_DADOS_H
+
+char* lerString(const char* msg);
+
+char* lerNome(void);
+
+char* lerCPF(void);
+
+char* lerNascimento(void);
+
+char* lerIdade(void);
+
+char* lerData(void);
+
+char* lerTelefone(void);
+
+char* lerEmail(void);
+
+float lerDinheiro(void);
+
+float lerSalario(void);
+
+#endif
