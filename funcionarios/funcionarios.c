5f972969be8024343f143db825e674bdbcebce22
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-10-28 10:50:31 -0300

    refactor + style: refinando lógica e melhorando o visual

--- funcionarios/funcionarios.c
+++ funcionarios/funcionarios.c
@@ -113,7 +113,7 @@ void telaPesquisarFuncionario(void){
 
 void telaListarFuncionarios(void){
     printf("╭──────────────────────────────────────────────╮\n");
-    printf("│              LISTAR FUNCIONÁRIOS             │\n");
+    printf("│             LISTA DE FUNCIONÁRIOS            │\n");
     listarFuncionarios();
     printf("╰──────────────────────────────────────────────╯\n");
     
@@ -124,23 +124,25 @@ void telaListarFuncionarios(void){
 
 void telaExcluirFuncionario(void){
     char op;
-    printf("╭──────────────────────────────────────────────╮\n");
-    printf("│              EXCLUIR FUNCIONÁRIO             │\n");
-    printf("├──────────────────────────────────────────────┤\n");
-    printf("│   [1] Exclusão Lógica                        │\n");
-    printf("│   [2] Exclusão definitiva                    │\n");
-    printf("│   [0] Sair                                   │\n");
-    printf("╰──────────────────────────────────────────────╯\n");
     do{
+        printf("╭──────────────────────────────────────────────╮\n");
+        printf("│              EXCLUIR FUNCIONÁRIO             │\n");
+        printf("├──────────────────────────────────────────────┤\n");
+        printf("│   [1] Exclusão Lógica                        │\n");
+        printf("│   [2] Exclusão definitiva                    │\n");
+        printf("│   [0] Sair                                   │\n");
+        printf("╰──────────────────────────────────────────────╯\n");
         op = opcao();
         switch (op)
         {
         case '1':
             excluirFuncionario();
+            op = '0';
             break;
 
         case '2':
             excluirFuncionarioDefinitivo();
+            op = '0';
             break;
 
         case '0':
@@ -151,9 +153,6 @@ void telaExcluirFuncionario(void){
             break;
         }
     }while (op != '0');
-
-    printf("|ENTER| para sair\n");
-
     esperarEnter();
     
 }
@@ -168,6 +167,7 @@ char telaOqueAtualizar(void){
     printf("│  [2] TELEFONE                                │\n");
     printf("│  [3] EMAIL                                   │\n");
     printf("│  [4] SALÁRIO                                 │\n");
+    printf("│  [0] SAIR                                    │\n");
     printf("╰──────────────────────────────────────────────╯\n");
     op = opcao();
     return op;
@@ -208,6 +208,17 @@ int cadastrarFuncionario(void){
 
     f->status = True;
 
+    printf("╭──────────────────────────────────────────────╮\n");
+    printf("│                  FUNCIONÁRIO                 │\n");
+    printf("├──────────────────────────────────────────────┤\n");
+    printf("│ Nome: %-38s │\n", f->nome);
+    printf("│ CPF: %-39s │\n", f->cpf);
+    printf("│ Data: %.2s/%.2s/%.4s                             │\n", f->nascimento, f->nascimento + 2, f->nascimento + 4);
+    printf("│ Telefone: (%.2s) %.1s %.4s-%.4s                   │\n",  f->telefone, f->telefone + 2,  f->telefone + 3, f->telefone + 7);
+    printf("│ E-mail: %-36s │\n", f->email);
+    printf("│ Salário: R$%-33.2f │\n", f->salario);
+    printf("╰──────────────────────────────────────────────╯\n");
+
     fun = fopen("funcionarios.dat", "ab");
     if (fun == NULL) {
         perror("Erro ao abrir o arquivo");
@@ -224,64 +235,78 @@ int cadastrarFuncionario(void){
 
 
 int atualizarFuncionario(void){
-    Funcionario* f;
-    f = (Funcionario*) malloc(sizeof(Funcionario));
+    FILE *fun;
+    Funcionario* f = (Funcionario*) malloc(sizeof(Funcionario));
     int encontrado = False;
-
     char quemAtualizar[20];
     char oqAtualizar;
 
-    FILE *fun;
-
     strcpy(quemAtualizar, lerCPF());
 
-    fun = fopen("funcionarios.dat","r+b");
-    while((fread(f, sizeof(Funcionario), 1, fun)) && (!encontrado)){
-        if((strcmp(quemAtualizar, f->cpf) == 0) && (f->status)){
-            do{
+    fun = fopen("funcionarios.dat", "r+b");
+    if (fun == NULL) {
+        perror("Erro ao abrir o arquivo");
+        free(f);
+        return 1;
+    }
+
+    while (fread(f, sizeof(Funcionario), 1, fun) == 1) {
+        if (strcmp(quemAtualizar, f->cpf) == 0) {
+            if (!f->status) {
+                printf("Não é possível atualizar dados (funcionário inativo).\n");
+                encontrado = True;
+                break;
+            }
+
+            do {
                 oqAtualizar = telaOqueAtualizar();
-                switch(oqAtualizar){
-                    case '1':
+                switch (oqAtualizar) {
+                    case '1': {
                         char *nome = lerNome();
                         strcpy(f->nome, nome);
                         free(nome);
                         break;
-
-                    case '2':
+                    }
+                    case '2': {
                         char *telefone = lerTelefone();
                         strcpy(f->telefone, telefone);
                         free(telefone);
                         break;
-                    
-                    case '3':
+                    }
+                    case '3': {
                         char *email = lerEmail();
                         strcpy(f->email, email);
                         free(email);
                         break;
-
+                    }
                     case '4':
                         f->salario = lerSalario();
                         break;
+                    case '0':
+                        break;
                     default:
-                        printf("invalido bobao\n");
+                        printf("Opção inválida. Tente novamente.\n");
                         break;
                 }
-            }while(oqAtualizar != '1' || oqAtualizar != '2' || oqAtualizar != '3' || oqAtualizar != '4');
-            fseek(fun, (-1)*sizeof(Funcionario), SEEK_CUR);
+            } while (oqAtualizar != '1' && oqAtualizar != '2' &&
+                     oqAtualizar != '3' && oqAtualizar != '4' && oqAtualizar != '0');
+
+            fseek(fun, -sizeof(Funcionario), SEEK_CUR);
             fwrite(f, sizeof(Funcionario), 1, fun);
             encontrado = True;
-        }
-        if((strcmp(quemAtualizar, f->cpf) == 0) && (!f->status)){
-            printf("Não é possível atualizar dados.");
-            encontrado = True;
+            if(oqAtualizar != '0'){
+                printf("Dados atualizados com sucesso!\n");
+            }
+            break;
         }
     }
+
+    if (!encontrado) {
+        printf("Funcionário não encontrado.\n");
+    }
+
     fclose(fun);
     free(f);
-    if(!encontrado){
-        printf("Nao encontrado");
-    }
-    
     return 0;
 }
 
@@ -302,7 +327,16 @@ int pesquisarFuncionario(void){
 
     while((fread(f, sizeof(Funcionario), 1, fun)) && (!encontrado)){
         if((strcmp(cpf, f->cpf) == 0) && (f->status)){
-            printf("NOME: %s\nCPF: %s\nDATA DE NASCIMENTO: %s\nTELEFONE: %s\nEMAIL: %s\nSALARIO: %.2f\n", f->nome, f->cpf, f->nascimento, f->telefone, f->email, f->salario);
+            printf("╭──────────────────────────────────────────────╮\n");
+            printf("│                  FUNCIONÁRIO                 │\n");
+            printf("├──────────────────────────────────────────────┤\n");
+            printf("│ Nome: %-38s │\n", f->nome);
+            printf("│ CPF: %-39s │\n", f->cpf);
+            printf("│ Data: %.2s/%.2s/%.4s                             │\n", f->nascimento, f->nascimento + 2, f->nascimento + 4);
+            printf("│ Telefone: (%.2s) %.1s %.4s-%.4s                   │\n",  f->telefone, f->telefone + 2,  f->telefone + 3, f->telefone + 7);
+            printf("│ E-mail: %-36s │\n", f->email);
+            printf("│ Salário: R$%-33.2f │\n", f->salario);
+            printf("╰──────────────────────────────────────────────╯\n");
             encontrado = True;
         } else if((strcmp(cpf, f->cpf) == 0) && (!f->status)){
             printf("Vinculo inativo, funcionario excluido\n");
@@ -326,7 +360,8 @@ int listarFuncionarios(void){
 
     fun = fopen("funcionarios.dat","rb");
     if(fun == NULL){
-        printf("Erro ao abrir o arquivo.");
+        printf("├──────────────────────────────────────────────┤\n");
+        printf("│           Erro ao abrir o arquivo.           │");
         free(f);
         return False;
     }
@@ -334,18 +369,19 @@ int listarFuncionarios(void){
     while(fread(f, sizeof(Funcionario), 1, fun)){
         if(f->status){
             printf("├──────────────────────────────────────────────┤\n");
-            printf("│ Nome: %-40s │\n", f->nome);
-            printf("│ CPF: %-41s │\n", f->cpf);
-            printf("│ Nascimento: %-34s │\n", f->nascimento);
-            printf("│ Telefone: %-36s │\n", f->telefone);
-            printf("│ E-mail: %-38s │\n", f->email);
-            printf("│ Salário: %-36.2f │\n", f->salario);
+            printf("│ Nome: %-38s │\n", f->nome);
+            printf("│ CPF: %-39s │\n", f->cpf);
+            printf("│ Data: %.2s/%.2s/%.4s                             │\n", f->nascimento, f->nascimento + 2, f->nascimento + 4);
+            printf("│ Telefone: (%.2s) %.1s %.4s-%.4s                   │\n",  f->telefone, f->telefone + 2,  f->telefone + 3, f->telefone + 7);
+            printf("│ E-mail: %-36s │\n", f->email);
+            printf("│ Salário: R$%-33.2f │\n", f->salario);
         }
     }
     fclose(fun);
     free(f);
     return 0;
 }
+
 
 
 
@@ -395,31 +431,32 @@ int excluirFuncionarioDefinitivo(void) {
     fun = fopen("funcionarios.dat", "rb");
     if (fun == NULL) {
         printf("Erro: não foi possível abrir o arquivo original.\n");
+        free(f);
         return 1;
     }
 
-    funTemp = fopen("temp.dat", "ab");
+    funTemp = fopen("temp.dat", "wb");
     if (funTemp == NULL) {
         printf("Erro: não foi possível criar o arquivo temporário.\n");
         fclose(fun);
+        free(f);
         return 1;
     }
 
     char *cpf = lerCPF();
 
-    while (fread(f, sizeof(Funcionario), 1, fun)) {
+    while (fread(f, sizeof(Funcionario), 1, fun) == 1) {
         if (strcmp(f->cpf, cpf) == 0) {
             encontrado = True;
             printf("Funcionário encontrado e excluído: %s\n", f->nome);
         } else {
-            fwrite(&f, sizeof(Funcionario), 1, funTemp);
+            fwrite(f, sizeof(Funcionario), 1, funTemp);
         }
     }
 
     fclose(fun);
     fclose(funTemp);
 
-    // Se encontrou, substitui o arquivo original
     if (encontrado) {
         remove("funcionarios.dat");
         rename("temp.dat", "funcionarios.dat");
@@ -428,6 +465,9 @@ int excluirFuncionarioDefinitivo(void) {
         remove("temp.dat");
         printf("Nenhum funcionário encontrado com o CPF informado.\n");
     }
+
+    free(f);
+    free(cpf);
 
     return 0;
 }
