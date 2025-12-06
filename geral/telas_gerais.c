280d8fcc28af0d913276182f6faa1a1419106199
leticiataylane <leticia.taylane.707@ufrn.edu.br>
2025-12-05 21:14:12 -0300

    style: Padronizando e colorindo telas gerais

--- geral/telas_gerais.c
+++ geral/telas_gerais.c
@@ -21,11 +21,9 @@ void telaInicial(void) {
 
         system("clear");
 
-        printf("╭─────────────────────────────────────────────────╮\n");
-        printf("|                                                 |\n");
-        printf("│          BEM-VINDO AO SISTEMA SIG-NAILS         │\n");
-        printf("|                                                 |\n");
-        printf("╰─────────────────────────────────────────────────╯\n");
+        printf(roxo negrito"╭─────────────────────────────────────────────────╮\n");
+        printf(            "│          BEM-VINDO AO SISTEMA SIG-NAILS         │\n");
+        printf(            "╰─────────────────────────────────────────────────╯\n"reset);
         sleep(2);
 }
 
@@ -36,17 +34,17 @@ char menuPrincipal(void) {
 
     system("clear");
     printf("\n");
-    printf("╭─────────────────────────────────────────────╮\n" );
-    printf("|           SIG-NAILS MENU PRINCIPAL          |\n" );
-    printf("├─────────────────────────────────────────────┤\n" );
-    printf("| 1. Agendamentos                             |\n" );
-    printf("| 2. Serviços                                 |\n" );
-    printf("| 3. Clientes                                 |\n" );
-    printf("| 4. Funcionários                             |\n" );
-    printf("| 5. Informações                              |\n" );
-    printf("| 6. Relatórios                               |\n" );
-    printf("| 0. Sair                                     |\n" );
-    printf("╰─────────────────────────────────────────────╯\n" );
+    printf("╭──────────────────────────────────────────────╮\n" );
+    printf("|           "roxo"SIG-NAILS MENU PRINCIPAL"reset"           |\n" );
+    printf("├──────────────────────────────────────────────┤\n" );
+    printf("| "roxo"[1] AGENDAMENTOS "reset"                            |\n" );
+    printf("| "roxo"[2] SERVIÇOS     "reset"                            |\n" );
+    printf("| "roxo"[3] CLIENTES     "reset"                            |\n" );
+    printf("| "roxo"[4] FUNCIONÁRIOS "reset"                            |\n" );
+    printf("| "roxo"[5] INFORMAÇÕES  "reset"                            |\n" );
+    printf("| "roxo"[6] RELATÓRIOS   "reset"                            |\n" );
+    printf("| "roxo"[0] SAIR         "reset"                            |\n" );
+    printf("╰──────────────────────────────────────────────╯\n" );
 
     op = opcao();
     return op;
@@ -58,49 +56,45 @@ void telaSobre(void) {
     system("clear"); 
     
     printf("\n");
-    printf("╭────────────────────────────────────────────╮\n" ); 
-    printf("|       SOBRE A EMPRESA E A EQUIPE           |\n" ); 
-    printf("╰────────────────────────────────────────────╯\n" );
-
-    printf("SIG-Nails ""é um sistema feito para\n");
-    printf("organizar serviços de manicure e pedicure,\n");
-    printf("trazendo praticidade para profissionais e clientes.\n\n");
-
-    printf("Equipe responsável: " "Letícia Taylane e Flávia Juliana\n" );
-    printf("──────────────────────────────────────────────\n" );
+    printf("╭──────────────────────────────────────────────────────╮\n"); 
+    printf("|             SOBRE A EMPRESA E A EQUIPE               |\n"); 
+    printf("├──────────────────────────────────────────────────────┤\n");
+    printf("| SIG-Nails é um sistema feito para                    |\n");
+    printf("| organizar serviços de manicure e pedicure, trazendo  |\n");
+    printf("| praticidade para profissionais e clientes.           |\n");
+    printf("├──────────────────────────────────────────────────────┤\n");
+    printf("| Equipe responsável: Letícia Taylane e Flávia Juliana |\n");
+    printf("╰──────────────────────────────────────────────────────╯\n");
 
     esperarEnter(); 
 }
-
-
-
-void telaFinal() {
-    system("clear");
-    printf("╭────────────────────────────────────────────╮\n");
-    printf("|         SIG-Nails - ENCERRAR               │\n");
-    printf("├────────────────────────────────────────────┤\n");
-    printf("│ Obrigada por utilizar o sistema!           │\n");
-    printf("│ Cuide-se e volte sempre!                   │\n");
-    printf("╰────────────────────────────────────────────╯\n");
-    esperarEnter();
-}
-
 
 void telaAjuda(void) {
     system("clear"); 
     printf("\n");
-    printf("╭────────────────────────────────────────────╮\n" ); 
-    printf("|              AJUDA E CONTATO               |\n" ); 
-    printf("╰────────────────────────────────────────────╯\n" );
-    printf(" Como navegar pelo sistema:\n" );
-    printf("- Use os números do menu principal para escolher as opções.\n");
-    printf("- Pressione ENTER quando solicitado para voltar.\n");
-    printf("- Evite usar teclas diferentes dos números indicados.\n\n");
-
-    printf(" Precisa de suporte?\n" );
-    printf("- Email: suporte@signails.com\n");
-    printf("- WhatsApp: (99) 99999-9999\n");
-    printf("- Instagram: @sig_nails\n");
-    printf("──────────────────────────────────────────────\n" );
+    printf("╭────────────────────────────────────────────────────────────╮\n"); 
+    printf("|                      AJUDA E CONTATO                       |\n"); 
+    printf("├────────────────────────────────────────────────────────────┤\n");
+    printf("| Como navegar pelo sistema:                                 |\n");
+    printf("|- Use os números do menu principal para escolher as opções. |\n");
+    printf("|- Pressione ENTER quando solicitado para voltar.            |\n");
+    printf("|- Evite usar teclas diferentes dos números indicados.       |\n");
+    printf("├────────────────────────────────────────────────────────────┤\n");
+    printf("| Precisa de suporte?                                        |\n");
+    printf("|- Email: suporte@signails.com                               |\n");
+    printf("|- WhatsApp: (99) 99999-9999                                 |\n");
+    printf("|- Instagram: @sig_nails                                     |\n");
+    printf("╰────────────────────────────────────────────────────────────╯\n");
     esperarEnter(); 
+}
+
+void telaFinal() {
+    system("clear");
+    printf(roxo negrito"╭────────────────────────────────────────────╮\n");
+    printf(            "|         SIG-Nails - ENCERRAR               │\n");
+    printf(            "├────────────────────────────────────────────┤\n");
+    printf(            "│ Obrigada por utilizar o sistema!           │\n");
+    printf(            "│ Cuide-se e volte sempre!                   │\n");
+    printf(            "╰────────────────────────────────────────────╯\n"reset);
+    esperarEnter();
 }
